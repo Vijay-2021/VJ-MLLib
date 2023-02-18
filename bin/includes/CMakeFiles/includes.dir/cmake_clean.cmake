0da9fe4a69a72e88d542ab8b8de69448543c63ea
Jacob Stolker <stolkerj@yahoo.com>
2023-02-18 01:59:32 +0000

    added basic structure

---
+++ bin/includes/CMakeFiles/includes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/includes.dir/matrix.cpp.o"
+  "CMakeFiles/includes.dir/matrix.cpp.o.d"
+  "libincludes.a"
+  "libincludes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/includes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
